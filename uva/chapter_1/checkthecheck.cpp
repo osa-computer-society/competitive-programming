23357004b0dec12915f708b4d0cffb15d6fed84e
Alexander Cai <alexandercai@outlook.com>
2019-06-13 17:43:42 -0600

    commented checkthecheck

--- uva/chapter_1/checkthecheck.cpp
+++ uva/chapter_1/checkthecheck.cpp
@@ -14,6 +14,7 @@ int game = 1;
 bool finished;
 char board[8][8];
 
+// Coordinates for how the pieces move
 const int rook_dr[4] = { 0, -1, 0, 1 };
 const int rook_dc[4] = { -1, 0, 1, 0 };
 const int bishop_dr[4] = { -1, 1, 1, -1 };
@@ -23,6 +24,7 @@ const int queen_dc[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 const int knight_dr[8] = { -2, -1, 1, 2, 2, 1, -1, -2 };
 const int knight_dc[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
+// Read in an 8x8 chessboard from the input and returns false if they're all blank
 bool get_board() {
   bool not_blank = false;
   for (int i = 0; i < 8; i++) {
@@ -34,6 +36,8 @@ bool get_board() {
   return not_blank;
 }
 
+// Checks if a king is in check and stops the current loop by setting finished to true.
+// I didn't use if/else if/else since it'll stop anyways when it returns.
 bool is_king(int r, int c, int color) {
   if (board[r][c] == 'K' && color == BLACK) {
     cout << "Game #" << game << ": white king is in check." << endl;
@@ -50,13 +54,13 @@ bool is_king(int r, int c, int color) {
   return false;
 }
 
+// Checks if a certain piece is attacking a king.
 void check(int r, int c) {
   int color = isupper(board[r][c]) ? WHITE : BLACK;
   int tr, tc;
 
   switch (board[r][c]) {
   case '.':
-    return;
     break;
 
   case 'P':
@@ -74,6 +78,8 @@ void check(int r, int c) {
   case 'R':
   case 'r':
     for (int i = 0; i < 4 && !finished; i++) { // 4 directions
+      // The board is only 8 wide so that's the longest we'll need to loop.
+      // We check along the whole path and break when it hits something
       for (int j = 1; j < 8; j++) {
         tr = r + j * rook_dr[i];
         tc = c + j * rook_dc[i];
@@ -127,8 +133,6 @@ void check(int r, int c) {
 
 int main() {
   while (get_board()) {
-    // Check the movements of each piece, including what it's blocked by
-    // If the king is in one of the squares it's attacking, we return that he's in check
     finished = false;
     for (int r = 0; r < 8 && !finished; r++)
       for (int c = 0; c < 8 && !finished; c++)
@@ -136,5 +140,6 @@ int main() {
     if (!finished) cout << "Game #" << game << ": no king is in check." << endl;
     game++;
   }
+  
   return 0;
 }
